968c8cf85b9c4024ee9a23acb74e6cae6069f473
Petr Nedoma <petr.nedoma@rws.com>
2019-02-26 14:13:58 +0100

    added error handling to install script

--- install.sh
+++ install.sh
@@ -5,7 +5,21 @@ DOTFILES_REPO="https://github.com/nnapik/dotfiles"
 APT_PACKAGES="git htop vim python youtube-dl zsh"
 SSH_EMAIL="nnapik@gmail.com"
 
+trap 'ret=$?; test $ret -ne 0 && printf "${red}Setup failed${reset}\n" >&2; exit $ret' EXIT
+set -e
 
+# --- Helpers
+print_success() {
+    printf "${green}✔ success:${reset} %b\n" "$1"
+}
+
+print_error() {
+    printf "${red}✖ error:${reset} %b\n" "$1"
+}
+
+print_info() {
+    printf "${blue}🛈 info:${reset} %b\n" "$1"
+}
 
 echo $PATH
 ln -sf $PATH/vimrc ~/.vimrc
