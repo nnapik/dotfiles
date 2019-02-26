85a7277717ca525b8dce51517dec6bae78958766
Petr Nedoma <nnapik@gmail.com>
2019-02-26 13:58:59 +0100

    first work on install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-ln -sf ~/.dotfiles/vimrc ~/.vimrc
-ln -sf ~/.dotfiles/bashrc ~/.bashrc
-ln -sf ~/.dotfiles/bash_aliases ~/.bash_aliases
+# sets up a linux and/or WSL based dev-env
+# inspired by elithrar/dotfiles
+DOTFILES_REPO="https://github.com/nnapik/dotfiles"
+APT_PACKAGES="git htop vim python youtube-dl zsh"
+SSH_EMAIL="nnapik@gmail.com"
+
+
+
+echo $PATH
+ln -sf $PATH/vimrc ~/.vimrc
+ln -sf $PATH/bashrc ~/.bashrc
+ln -sf $PATH/bash_aliases ~/.bash_aliases
